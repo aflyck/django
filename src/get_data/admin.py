56687ae97f2fe25a24fcc32804faa866005fed17
aflyk <aflyck@gmail.com>
2022-08-03 17:29:08 +1100

    add new class ProgramLanguage and register it admin.py

--- src/get_data/admin.py
+++ src/get_data/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import City, ProgramLanguage
 # Register your models here.
+
+
+@admin.register(City)
+class AdminCity(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ["name"]}
+
+
+@admin.register(ProgramLanguage)
+class AdminProgramLanguage(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['name']}
--- src/get_data/models.py
+++ src/get_data/models.py
@@ -4,5 +4,28 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(blank=True)
+    name = models.CharField(max_length=100,
+                            verbose_name="Наименование населенного пункта",
+                            unique=True)
+    slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Наименование населенного пункта'
+        verbose_name_plural = "Наименование населенных пунктов"
+
+
+class ProgramLanguage(models.Model):
+    name = models.CharField(max_length=100,
+                            verbose_name="Язык программирования",
+                            unique=True)
+    slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = "Языки программирования"
