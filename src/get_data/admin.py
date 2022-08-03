f23e06186af76be714aa98a516a307a5322551c3
aflyk <aflyck@gmail.com>
2022-08-03 18:15:43 +1100

    add new class Vacancy and reg it

--- src/get_data/admin.py
+++ src/get_data/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import City, ProgramLanguage
+from .models import City, ProgramLanguage, Vacancy
 # Register your models here.
 
 
@@ -11,3 +11,8 @@ class AdminCity(admin.ModelAdmin):
 @admin.register(ProgramLanguage)
 class AdminProgramLanguage(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
+
+
+@admin.register(Vacancy)
+class AdminVacancy(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['title']}
--- src/get_data/models.py
+++ src/get_data/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -29,3 +30,22 @@ class ProgramLanguage(models.Model):
     class Meta:
         verbose_name = 'Язык программирования'
         verbose_name_plural = "Языки программирования"
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name="Заголовок вакансии")
+    company = models.CharField(max_length=250, verbose_name="Компания")
+    description = models.TextField(verbose_name="Описание вакансии")
+    city = models.ForeignKey('City', on_delete=models.CASCADE,
+                             verbose_name="Город")
+    program_language = models.ForeignKey('ProgramLanguage', on_delete=models.CASCADE,
+                                         verbose_name="Язык программирования")
+    slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.city}'
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
